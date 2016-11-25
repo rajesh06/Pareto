ba11ec50f066f92de56769eda22297f14487de87
OWG\RAJESH.SAHASRABUDDHE <rajesh.sahasrabuddhe@oliverwyman.com>
2016-11-25 11:46:58 -0500

    Cont Int

--- pareto_2.R
+++ pareto_2.R
@@ -20,18 +20,40 @@ plot(y = qs, x = seq(from = 1, to = 1.5, by = .001))
 
 
 plot(x = pareto_df$pareto_x, y = pareto_df$probs, xlim = c(0, 10),
-  xlab = "x", ylab = "F(x)")
+  xlab = "x", ylab = "F(x)", lab = 'Percentile')
 lines(x = pareto_df$pareto_x, y = pareto_df$probs, col = "red")
 
 
-params <- list(q = 1.5)
-tail_wt <- data_frame(ptile = seq(from = 0.00, to = 0.99, by =.01)) %>%
-  dplyr::mutate(pareto_x = (1 - ptile)^(-1 / params$q))
-tw_plot_y <- (tail_wt$pareto_x[2:100] - tail_wt$pareto_x[1:99]) /
-  (tail_wt$ptile[2:100] - tail_wt$ptile[1:99])
-tw_plot_x <- tail_wt$ptile[1:99]
-plot(xlim = c(0,1), ylim = c(0,110),   x = tw_plot_x, y = tw_plot_y, type = 'l')
+plot(x = 1, xlim = c(0,1), ylim = c(0, 2), type = 'n', 
+  ylab = 'Change in F(x)', xlab = 'Percentile')
+text_y = c(1.8, 1.6, 1.4)
+my_colors <- c("red", "blue", "green")
+  
+params <- list(q1 = 0.50, q2 = 1, q3 = 3)
 
+sapply(X = params, FUN = function(q) {
+  tail_wt <- data_frame(ptile = seq(from = 0.00, to = 0.99, by =.01)) %>%
+    dplyr::mutate(pareto_x = (1 - ptile)^(-1 / q))
+  tw_plot_y <- (tail_wt$pareto_x[2:100] / tail_wt$pareto_x[1:99])
+  tw_plot_x <- tail_wt$ptile[1:99]
+  my_col = my_colors[q == params]
+  lines(x = tw_plot_x, y = tw_plot_y, type = 'l', col = my_col)
+  text(x = 0.1, y = text_y[q == params], label = paste('q = ', q),
+    col = my_col, adj = 0)
+  return(NULL)
+})
 
+set.seed(12345)
+lnorm_sim <- rlnorm(n = 500, meanlog = 10, sdlog = 2)
+#hist(log(lnorm_sim))
+lnorm_sim <- lnorm_sim[order(lnorm_sim)]
+#lnorm_sim <- (lnorm_sim/25000)[which(lnorm_sim > 25000)]
 
+no_xs <- length(lnorm_sim)
+lnorm_x <- seq(from = 0, to = 1, by = 1 / no_xs)
+lnorm_x <- lnorm_x[2:no_xs]
+
+lnorm_y <- lnorm_sim[2:no_xs] / lnorm_sim[1:(no_xs - 1)]
+
+points(x = lnorm_x, y = lnorm_y)
 
