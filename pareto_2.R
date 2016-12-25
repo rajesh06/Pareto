d0673f8dbffaae1ed186c9e40e601e4ac3013d10
OWG\RAJESH.SAHASRABUDDHE <rajesh.sahasrabuddhe@oliverwyman.com>
2016-12-24 23:18:17 -0500

    Continuous Development

--- pareto_2.R
+++ pareto_2.R
@@ -24,7 +24,7 @@ plot(x = pareto_df$pareto_x, y = pareto_df$probs, xlim = c(0, 10),
 lines(x = pareto_df$pareto_x, y = pareto_df$probs, col = "red")
 
 
-plot(x = 1, xlim = c(0,1), ylim = c(0, 2), type = 'n', 
+plot(x = 1, xlim = c(0,1), ylim = c(.5, 2), type = 'n', 
   ylab = 'Change in F(x)', xlab = 'Percentile')
 text_y = c(1.8, 1.6, 1.4)
 my_colors <- c("red", "blue", "green")
@@ -43,6 +43,24 @@ sapply(X = params, FUN = function(q) {
   return(NULL)
 })
 
+params <- list(mu1 = 10, sigma1 = 2)
+
+sapply(X = params, FUN = function(q) {
+  tail_wt <- data_frame(ptile = seq(from = 0.00, to = 0.99, by =.01)) %>%
+    dplyr::mutate(lnorm_x = qlnorm(p = ptile, meanlog = params$mu1,
+      sdlog = params$sigma1))
+  tw_plot_y <- (tail_wt$lnorm_x[2:100] / tail_wt$lnorm_x[1:99])
+  tw_plot_x <- tail_wt$ptile[1:99]
+  my_col = 'blue'
+  lines(x = tw_plot_x, y = tw_plot_y, type = 'l', col = my_col)
+  text(x = 0.1, y = text_y[q == params], label = paste('q = ', q),
+    col = my_col, adj = 0)
+  return(NULL)
+})
+
+
+
+
 set.seed(12345)
 lnorm_sim <- rlnorm(n = 500, meanlog = 10, sdlog = 2)
 #hist(log(lnorm_sim))
@@ -55,5 +73,5 @@ lnorm_x <- lnorm_x[2:no_xs]
 
 lnorm_y <- lnorm_sim[2:no_xs] / lnorm_sim[1:(no_xs - 1)]
 
-points(x = lnorm_x, y = lnorm_y)
+#points(x = lnorm_x, y = lnorm_y)
 
