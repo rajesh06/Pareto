74c84a7fdac6be953a5e91d7baf3845d36ab1a72
rajesh06 <rajesh1004@gmail.com>
2017-06-04 23:23:53 -0400

    start again

--- tail_wt_plot.R
+++ tail_wt_plot.R
@@ -1,33 +1,33 @@
-tail_wt_df <- data.frame(
-   = 1:100 / 100
+no_points <- 25
 
-)
-
-library(magrittr)
-no_points <- 100
-
-pareto_q <- 1.5
+pareto_qs <- c(1.1, 1.5, 2.0, 3.0)
 set.seed(12345)
 
-ptiles <- runif(n = no_points, min = 0, max = 1)
+dev.off()
 
-data_Fx <- sort(ptiles)
-data_x <- (1 - data_Fx) ^ (-1 / pareto_q)
+par(mfrow = c(2,2))
 
-model_tw <- (data_Fx[2:no_points] - data_Fx[1:(no_points - 1)]) /
-  (data_x[2:no_points] - data_x[1:(no_points - 1)])
+sapply(X = pareto_qs, FUN = function(pareto_q){
+  # simulate via inversion
+  ptiles <- runif(n = no_points, min = 0, max = 1)
 
-model_tw <- (1 / no_points) / (
-  (1 - (2:100)/100) ^ (-1/pareto_q) - (1 - (1:99)/100) ^ (-1/pareto_q)
-  )
+  data_Fx <- sort(ptiles)
+  data_x <- (1 - data_Fx) ^ (-1 / pareto_q)
 
-data_tw <- (1 / no_points) /
-  (data_x[2:no_points] - data_x[1:(no_points - 1)])
-#data_x[1:(no_points - 1)]
-plot(x = 1:99, y = data_tw, ylim = c(0,2))
-lines(x = 1:99, y = model_tw, col = 'red')
-points(x = 1:99, y = model_tw, col = 'red')
+  model_Fx <- seq(from = 0, to = 1, length.out = no_points)
+  model_x <- (1 - model_Fx) ^ (-1 / pareto_q)
 
+  data_tw <- (data_Fx[2:no_points] - data_Fx[1:(no_points - 1)]) /
+    (data_x[2:no_points] - data_x[1:(no_points - 1)])
 
+  model_tw <- (model_Fx[2:no_points] - model_Fx[1:(no_points - 1)]) /
+    (model_x[2:no_points] - model_x[1:(no_points - 1)])
 
+  plot(x = data_x[1:(no_points - 1)], y = data_tw, ylim = c(0,2),
+    ylab = 'Tail Weight', xlab = 'Normalized Claim Size')
+  lines(x = model_x[1:(no_points - 1)], y = model_tw, col = 'red')
+  text(x = mean(model_x[1:(no_points - 1)]), y = 2,
+    labels = paste('q = ', pareto_q, '( n = ', no_points, ')'), adj = c(0,1))
+  return(NULL)
+}, simplify = TRUE)
 
